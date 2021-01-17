64e1f538c7017f47bdfd02982513ccc14cc909aa
abelanico6@gmail.com <abelanico6@gmail.com>
2021-01-17 07:10:34 +0100

    Initial commit

---
+++ Demo API2/Controllers/DemoSandboxController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Demo_API2.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo_API2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoSandboxController : ControllerBase
+    {
+        DemoSandboxService Srv = new DemoSandboxService();
+
+        // GET: api/DemoSandbox
+        [HttpGet]
+        public IEnumerable<UserDTO> Get()
+        {
+            return Srv.GetAll();
+        }
+
+        // GET: api/DemoSandbox/5
+        [HttpGet("{id}", Name = "Get")]
+        public UserDTO Get(int id)
+        {
+            return Srv.GetOneById(id);
+        }
+
+        // POST: api/DemoSandbox
+        [HttpPost]
+        public UserDTO Post([FromBody] UserDTO value)
+        {
+            return Srv.AddUser(value);
+        }
+
+        // PUT: api/DemoSandbox/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public CustomResponse Delete(int id)
+        {
+            return Srv.DeleteUser(id);
+        }
+    }
+}
---
+++ Demo API2/Services/DemoSandbox.service.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo_API2.Services
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public List<String> Tags = new List<string>();
+    }
+
+    public class CustomResponse
+    {
+        public string Message { get; set; }
+
+        public bool IsSuccessful { get; set; }
+    }
+
+    public class DemoSandboxService
+    {
+        private static List<UserDTO> UserList = new List<UserDTO>();
+
+        public UserDTO GetOneById(int id)
+        {
+            return UserList.Find((user) => user.Id == id);
+        }
+
+        public UserDTO AddUser(UserDTO userData)
+        {
+            try
+            {
+                UserDTO userExists = UserList.Find((user) => user.Id == userData.Id);
+                if (userExists != null)
+                {
+                    throw new Exception("This user already exists");
+                }
+                UserList.Add(userData);
+                return userData;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<UserDTO> GetAll()
+        {
+            try
+            {
+                return UserList;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public CustomResponse DeleteUser(int id)
+        {
+            try
+            {
+                List<UserDTO> allUsers = UserList.Where((user) => user.Id != id).ToList();
+
+                UserList = allUsers;
+                return new CustomResponse()
+                {
+                    IsSuccessful = true,
+                    Message = "Delete works"
+                };
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
